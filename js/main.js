16256b48ccb0c925b820b5c17760f39eca8924e6
Samuel Odira <52766763+czsamzzy@users.noreply.github.com>
2019-12-02 11:16:51 +0100

    site design completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 
+ $(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      544: {
+        items: 2
+      }
+    }
+  });
+});
+
 $(function() {
   var $container = $(".gallery-group");
 
@@ -63,3 +79,19 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 //================================================
+
+//sticky nav menu
+let nav_offset_top = $(".header_area").height() + 50;
+
+function navbarFixed() {
+  if ($(".header_area").length) {
+    $(window).scroll(function() {
+      let scroll = $(window).scrollTop();
+      if (scroll >= nav_offset_top) {
+        $(".header_area .main-menu").addClass("navbar_fixed");
+      } else {
+        $(".header_area .main-menu").removeClass("navbar_fixed");
+      }
+    });
+  }
+}
